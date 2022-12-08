85b4166e5f95bee0cbf386efdd1769a6007d2dd3
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-12-07 22:40:01 -0800

    minor changes to query.sql

--- db/query.sql
+++ db/query.sql
@@ -1,6 +1,6 @@
-SELECT employees.id, employees.first_name, employees.last_name, roles.employee_title, departments.dept_name, roles.salary, CONCAT(e2.first_name, " ", e2.last_name) AS manager  
+SELECT employees.id, employees.first_name, employees.last_name, roles.employee_title AS title, departments.dept_name AS department, roles.salary, CONCAT(e2.first_name, " ", e2.last_name) AS manager  
 
-FROM employees FULL OUTER JOIN employees e2 ON e2.id = employees.manager_id
+FROM employees LEFT JOIN employees e2 ON e2.id = employees.manager_id
                JOIN roles ON employees.role_id = roles.id
                JOIN departments ON roles.department_id = departments.id
                
